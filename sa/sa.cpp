917a5cff603c1c43de17511234af8b7c35827bb8
cppascalinux <cppascalinux@gmail.com>
2019-04-09 12:56:36 +0800

    suffix array

--- sa/sa.cpp
+++ sa/sa.cpp
@@ -1,26 +1,41 @@
+//2019.04.05
+//uoj35 后缀排序
 #include<iostream>
 #include<cstdio>
 #include<cstring>
 #include<algorithm>
 using namespace std;
-int n,r;
-char p[10010][60];
-char s[1000010];
-int t1[1000010],t2[1000010],sa[1000010],c[30],rank[1000010],k[1000010];
-void build()
+int n;
+char s[100009];
+int sa[100009],t1[100009],t2[100009],rnk[100009],h[100009],c[100009];
+void geth()
+{
+	for(int i=1;i<=n;i++)
+		rnk[sa[i]]=i;
+	int p=0;
+	for(int i=1;i<=n;i++)
+	{
+		if(p)
+			p--;
+		int np=sa[rnk[i]-1];
+		while(s[i+p]==s[np+p])
+			p++;
+		h[rnk[i]]=p;
+	}
+}
+void getsa()
 {
 	int *x=t1,*y=t2;
-	m=26;
-	n=strlen(s);
-	for(int i=n;i>=1;i--)
-			s[i]=s[i-1]-'a'+1;
-	memset(c,0,sizeof(c));
+	int m=26;
 	for(int i=1;i<=n;i++)
-			c[x[i]=s[i]]++;
+		c[x[i]=s[i]]++;
 	for(int i=1;i<=m;i++)
-			c[i]+=c[i-1];
-	for(int i=n;i>=1;i--)
+		c[i]+=c[i-1];
+	for(int i=1;i<=n;i++)
 		sa[c[x[i]]--]=i;
+	// for(int i=1;i<=n;i++)
+	// 	printf("%d ",sa[i]);
+	// printf("\n");
 	for(int k=1;k<=n;k<<=1)
 	{
 		int p=0;
@@ -29,41 +44,41 @@ void build()
 		for(int i=1;i<=n;i++)
 			if(sa[i]>=k+1)
 				y[++p]=sa[i]-k;
-		memset(c,0,sizeof(c));
+		// for(int i=1;i<=n;i++)
+		// 	printf("%d ",y[i]);
+		// printf("\n");
+		memset(c,0,(m+1)<<2);
 		for(int i=1;i<=n;i++)
-			c[x[y[i]]]++;
+			c[x[i]]++;
 		for(int i=1;i<=m;i++)
 			c[i]+=c[i-1];
 		for(int i=n;i>=1;i--)
 			sa[c[x[y[i]]]--]=y[i];
-		swap(x,y);
 		p=0;
 		for(int i=1;i<=n;i++)
-			x[sa[i]]=y[sa[i]]==y[sa[i-1]]&&y[sa[i]+k]==y[sa[i-1]+k]?p:++p;
-		if(p>=n)
-			break;
+			y[sa[i]]=x[sa[i]]==x[sa[i-1]]&&x[sa[i]+k]==x[sa[i-1]+k]?p:++p;
+		swap(x,y);
 		m=p;
+		if(m>=n)
+			break;
 	}
 }
 int main()
 {
-	#ifndef ONLINE_JUDGE
+#ifndef ONLINE_JUDGE
 	freopen("sa.in","r",stdin);
 	freopen("sa.out","w",stdout);
-	#endif
-	int t;
-	scanf("%d",&t);
-	while(t--)
-	{
-		scanf("%d",&r);
-		for(int i=1;i<=r;i++)
-			scanf("%s",p[i]);
-		scanf("%s",s);
-		build();
-		for(int i=1;i<=r;i++)
-			match(p[i]);
-		printf("%d\n",ans);
-		clr();
-	}
+#endif
+	scanf("%s",s+1);
+	n=strlen(s+1);
+	for(int i=1;i<=n;i++)
+		s[i]-='a'-1;
+	getsa();
+	geth();
+	for(int i=1;i<=n;i++)
+		printf("%d ",sa[i]);
+	printf("\n");
+	for(int i=2;i<=n;i++)
+		printf("%d ",h[i]);
 	return 0;
 }
