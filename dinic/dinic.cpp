fd642d0d4425ad2382d96c5b5d2b24031a474a85
cppascalinux <cppascalinux@gmail.com>
2019-06-11 14:29:09 +0800

    rewrote dinic

--- dinic/dinic.cpp
+++ dinic/dinic.cpp
@@ -1,85 +1,79 @@
+//2019.06.11
+//loj101 最大流 dinic
 #include<iostream>
 #include<cstdio>
 #include<cstring>
 #include<algorithm>
 #include<queue>
+#define inf 0x7FFFFFFFFFFFFFFFLL
+#define li long long
 using namespace std;
-const int inf=0x7FFFFFFF;
 int n,m,s,t,tot=1;
-int head[10010],cur[10010],d[10010];
-int edge[1000010],next[1000010],cap[1000010];
-queue<int> q;
+int hd[109],eg[10009],nxt[10009],cap[10009];
+int cur[109],dep[109];
 void ins(int a,int b,int c)
 {
-    edge[++tot]=b;
-    cap[tot]=c;
-    next[tot]=head[a];
-    head[a]=tot;
-    edge[++tot]=a;
-    cap[tot]=0;
-    next[tot]=head[b];
-    head[b]=tot;
+    eg[++tot]=b,cap[tot]=c,nxt[tot]=hd[a],hd[a]=tot;
+    eg[++tot]=a,cap[tot]=0,nxt[tot]=hd[b],hd[b]=tot;
 }
 int bfs()
 {
-    int x;
-    memset(d,0,sizeof(d));
-    d[s]=1;
+    queue<int> q;
+    memset(dep,0,sizeof(dep));
+    dep[s]=1;
     q.push(s);
     while(!q.empty())
     {
-        x=q.front();
+        int x=q.front();
         q.pop();
-        for(int i=head[x];i;i=next[i])
-            if(!d[edge[i]]&&cap[i])
+        for(int i=hd[x];i;i=nxt[i])
+            if(cap[i]&&!dep[eg[i]])
             {
-                d[edge[i]]=d[x]+1;
-                q.push(edge[i]);
+                dep[eg[i]]=dep[x]+1;
+                q.push(eg[i]);
             }
     }
-    return d[t];
+    return dep[t];
 }
-int dfs(int x,int a)
+li dfs(int x,li a)
 {
-    if(x==t||a==0)
+    if(x==t||!a)
         return a;
-    int f,totf=0;
-    for(int &i=cur[x];i;i=next[i])
-        if(d[edge[i]]==d[x]+1&&(f=dfs(edge[i],min(a,cap[i]))))
+    li totf=0;
+    int f;
+    for(int &i=cur[x];i;i=nxt[i])
+        if(cap[i]&&dep[eg[i]]==dep[x]+1&&(f=dfs(eg[i],min(a,(li)cap[i]))))
         {
-            cap[i]-=f;
-            cap[i^1]+=f;
-            a-=f;
-            totf+=f;
+            cap[i]-=f,cap[i^1]+=f;
+            a-=f,totf+=f;
             if(!a)
                 break;
         }
     return totf;
 }
-int flow()
+li flow()
 {
-    int totf=0;
+    li totf=0;
     while(bfs())
     {
         for(int i=1;i<=n;i++)
-            cur[i]=head[i];
+            cur[i]=hd[i];
         totf+=dfs(s,inf);
     }
     return totf;
 }
 int main()
 {
-	#ifndef ONLINE_JUDGE
-	freopen("dinic.in","r",stdin);
-	freopen("dinic.out","w",stdout);
-	#endif
+#ifdef I_LOVE_KTY
+    freopen("dinic.in","r",stdin);
+    freopen("dinic.out","w",stdout);
+#endif
     scanf("%d%d%d%d",&n,&m,&s,&t);
-    for(int i=1;i<=m;i++)
+    for(int i=1,a,b,c;i<=m;i++)
     {
-        int a,b,c;
         scanf("%d%d%d",&a,&b,&c);
         ins(a,b,c);
     }
-    printf("%d\n",flow());
+    printf("%lld",flow());
     return 0;
 }
